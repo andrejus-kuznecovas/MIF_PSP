f9638109b6d8c1cb0fa03121ec3ae38f744a2ddd
Andrej Kuznecov <aku@tiatechnology.com>
2018-10-01 17:04:30 +0300

    start

--- scala2/src/Main.scala
+++ scala2/src/Main.scala
@@ -1,6 +1,19 @@
-object HelloWorld {
+object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
 }
 
+
+
+trait ConstructionZoneBoardMixin {
+  def performConstruction();
+
+}
+class CityConstructionZoneBoard(buildingCount: Int) extends ConstructionZoneBoardMixin with ConstructionZoneShareHolderMixin{
+  override def performConstruction(): Unit = ???
+}
+class ProvinceConstructionZone() extends ConstructionZoneBoardMixin with ConstructionZoneShareHolderMixin {
+  override def performConstruction(): Unit = ???
+}
+trait ConstructionZoneShareHolderMixin {}
